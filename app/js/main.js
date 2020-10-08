4b1e74bfa448b6245bba92d54dce69eef3284c22
EA-hub <your_email@whatever.com>
2020-10-08 20:53:33 +0300

    Добавили к стартовой сборке Gulp animate.css;сверстали блок header. Слайдер с помошью Slick.Карта на сайте вставлена кодом, прописана анимация для координат на карте и появление маршрута.

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+$('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow right"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow right"></img>',
+    asNavFor: '.slider-dots'
+});
+
+$('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider'
+});
+    
+});
