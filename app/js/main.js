aca27a089ea176db8e6214942d1c62e579fa196b
EA-hub <your_email@whatever.com>
2020-10-10 21:49:01 +0300

    Добавли блок SURF и стили к нему+js. Слайдер с помощью slick. Точки как координаты

--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,34 @@ $('.header__slider').slick({
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow right"></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow right"></img>',
-    asNavFor: '.slider-dots'
+    asNavFor: '.slider-dotshead'
 });
 
-$('.slider-dots').slick({
+$('.slider-dotshead').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
-    asNavFor: '.header__slider'
+    arrows: false,
+    asNavFor: '.header__slider',
+    focusOnSelect: true
 });
+
+$('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    centerMode: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow right"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow right"></img>',
+    asNavFor: '.slider-map',
+    focusOnSelect: true
+});
+
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
+    focusOnSelect: true
+});
+
     
 });
